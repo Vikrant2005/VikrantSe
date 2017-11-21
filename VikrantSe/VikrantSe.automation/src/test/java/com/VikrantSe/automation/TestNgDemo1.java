50acc72d2ca242d631c92ee6983fc55e583bcbc1
Vikrant2005 <30979993+Vikrant2005@users.noreply.github.com>
2017-11-21 22:04:08 +0000

    Add files via upload

---
+++ VikrantSe/VikrantSe.automation/src/test/java/com/VikrantSe/automation/TestNgDemo1.java
@@ -0,0 +1,14 @@
+package com.VikrantSe.automation;
+
+import org.testng.annotations.Test;
+
+public class TestNgDemo1 {
+	
+	@Test
+	public void testEnvironment()
+	{
+		System.out.println("Environment Set");
+	}
+	
+
+}
